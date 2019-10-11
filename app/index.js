9c6179b92617443c1adea22c6dc742cfcbb8fbc8
Rogerio Angeliski <angeliski@hotmail.com>
2019-10-11 09:17:28 -0300

    Refactor and create overlay effect

--- app/index.js
+++ app/index.js
@@ -1,4 +1,11 @@
 (function(){
-    console.log('Init app');
+    console.log('Init app')
+
+    const $quote = document.getElementById('quote')
+    $quote.innerHTML = "\"Design não é apenas o que parece ou o que se sente. Design é como funciona.\""
+
     
+    const $author = document.getElementById('author')
+    $author.innerHTML = "&#8212; Steve jobs"
+
 })()
