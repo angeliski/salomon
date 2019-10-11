8d0810a53e5d6fffaac3d466a06bcf00a000432d
Rogerio Angeliski <angeliski@hotmail.com>
2019-10-11 11:33:54 -0300

    Adicionando sistema simplificado de request

--- app/index.js
+++ app/index.js
@@ -1,11 +1,45 @@
 (function(){
     console.log('Init app')
+    let global = [];
 
-    const $quote = document.getElementById('quote')
-    $quote.innerHTML = "\"Design não é apenas o que parece ou o que se sente. Design é como funciona.\""
+    const save = (quotes) => {
+        global = quotes;
+    }
 
+    const getOne = () => {
+        return global[0];
+    }
+
+    const render = (quote) => {
+        const $quote = document.getElementById('quote')
+        $quote.innerHTML = `"${quote.quote}"`
     
-    const $author = document.getElementById('author')
-    $author.innerHTML = "&#8212; Steve jobs"
+        const $author = document.getElementById('author')
+        $author.innerHTML = `&#8212; ${quote.author}`
+    
+    }
+
+    const ajax = (url) => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest()
+            xhr.open("GET", chrome.extension.getURL(url), true);
+            xhr.onreadystatechange = () => {
+              if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(new Error("Problemas ao carregar as frases."));
+                }
+              }
+            };
+            xhr.send();
+        });
+      };
+
+
+      ajax('./data/quotes.json')
+      .then(save)
+      .then(getOne)
+      .then(render);
 
 })()
